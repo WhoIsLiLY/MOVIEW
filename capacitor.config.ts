c27c2196c520457ee538b70a504a88dac18bde67
WhoIsLiLY <127612137+WhoIsLiLY@users.noreply.github.com>
2025-03-31 21:48:07 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'project_hmp_160422011_160422007',
+  webDir: 'www'
+};
+
+export default config;
